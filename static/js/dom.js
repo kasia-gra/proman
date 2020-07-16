b8da5216926dcdfa94e3aede646c563491fe3683
bartcapote <gauclee@gmail.com>
2020-07-16 21:18:14 +0200

    Fix incorrect id's for numbers 10+

--- static/js/dom.js
+++ static/js/dom.js
@@ -137,7 +137,7 @@ function addNewStatusListeners() {
         if (e.target.classList.contains('board-add-status')) {
             $('#modal-create-status').modal()
             const board = e.target.parentNode.parentNode
-            const boardId = board.id[board.id.length - 1];
+            const boardId = board.id.replace( /^\D+/g, '');
             modal.setAttribute('board', boardId)
         }
     }
