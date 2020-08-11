9b1d28d2bad64cb23d1aa502d16a030c4a247fe9
kasia-gra <grabowska.kat@gmail.com>
2020-08-11 12:59:07 +0200

    Modified updating new board title

--- static/js/change_board_name.js
+++ static/js/change_board_name.js
@@ -3,35 +3,18 @@ import {dataHandler} from "./data_handler.js";
 
 export let changeBoardName = {
     addEventListenersToBoardTitles: function () {
-        document.addEventListener('click', function (e) {
-            if (e.target && e.target.className == 'board-title-input') {
-                let isDataPosted = localStorage.setItem("posted", "false");
-                let currentElement = e.target;
-                currentElement.id = "board-title-to-post"
-                // let editedBoardId = e.target.parentNode.parentNode.id;
-                // let newBoardTitle = e.target.value;
-                postData(currentElement)
-            }
-        })
+        const boardsContainer = document.querySelector(".board-container");
+        boardsContainer.addEventListener('change', updateValue);
     }
 }
 
-
-function postData(currentElement) {
-    window.onclick = function (event) {
-        let isDataPosted = localStorage.getItem("posted")
-        if (event.target != currentElement && isDataPosted == "false") {
-            let isDataPosted = localStorage.setItem("posted", "true");
-            let textWithData = document.getElementById("board-title-to-post");
-            let newTitle = textWithData.value;
-            let boardId = textWithData.parentNode.parentNode.id.replace("header-board-", "");
-            let dataToPost = {};
-            dataToPost["id"] = parseInt(boardId);
-            dataToPost["title"] = newTitle;
-            textWithData.removeAttribute('id');
-            dataHandler.editBoard(dataToPost, function (edited_board_data) {
-                console.log(edited_board_data)
-            })
-        }
+const updateValue = function (e) {
+    const dataToPost = {};
+    if (e.target.className == "board-title-input") {
+        console.log("updating board title");
+        dataToPost["title"] = e.target.value;
+        dataToPost["id"] = e.target.parentNode.parentNode.id.replace("header-board-", "");
+        dataHandler.editBoard(dataToPost, function (edited_board_data) {
+        })
     }
 }
